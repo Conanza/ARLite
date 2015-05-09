08aad0bd9800ca7eb71aea6508be768d65550e95
Conan Tzou <conantzou@gmail.com>
2015-05-09 01:11:25 -0700

    Finish belongs_to and has_many method writers

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -9,21 +9,22 @@ class AssocOptions
     :primary_key
   )
 
-  
+  # Returns associated object's class
   def model_class
     class_name.constantize
   end
 
+  # Returns associated object's table name
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     defaults = {
-      foreign_key: "#{name.singularize}_id".to_sym,
-      class_name: name.camelcase,
+      foreign_key: "#{name.to_s.singularize}_id".to_sym,
+      class_name: name.to_s.camelcase,
       primary_key: :id
     }.merge(options)
 
@@ -36,7 +37,7 @@ class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     defaults = {
       foreign_key: "#{self_class_name.downcase}_id".to_sym,
-      class_name: name.singularize.camelcase,
+      class_name: name.to_s.singularize.camelcase,
       primary_key: :id
     }.merge(options)
 
@@ -48,11 +49,21 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    opts = BelongsToOptions.new(name, options)
+
+    define_method(name.to_sym) do 
+      object = opts.model_class.where(id: self.send(opts.foreign_key))
+
+      object.first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    opts = HasManyOptions.new(name, self.name, options)
+
+    define_method(name.to_sym) do 
+      opts.model_class.where(opts.foreign_key => self.send(opts.primary_key))
+    end
   end
 
   def assoc_options
